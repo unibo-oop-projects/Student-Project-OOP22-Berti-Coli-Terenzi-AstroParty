10b72d8fcce4c3d28c6b4e507b796553588593a4
AlessandroColi <alessandro.coli2@studio.unibo.it>
2023-03-04 11:01:41 +0100

    Spaceship: implementazione Builder

---
+++ src/main/java/it/unibo/AstroParty/model/Spaceship/impl/SpaceshipBuilderImpl.java
@@ -0,0 +1,55 @@
+package it.unibo.AstroParty.model.Spaceship.impl;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import it.unibo.AstroParty.model.Spaceship.api.SpaceshipBuilder;
+import it.unibo.AstroParty.model.api.Spaceship;
+
+public class SpaceshipBuilderImpl implements SpaceshipBuilder {
+	
+	private double speed;
+	private int maxBullets;
+	private double time;
+	private boolean shield;
+	
+	SpaceshipBuilderImpl(){
+										//TODO usa un file .yml per salvare i default
+		this.setSpeed(2);
+		this.setStartingShield(false);
+		this.setRechargeTime(3);
+		this.setMaxBullets(3);		
+	}
+
+	@Override
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+	@Override
+	public void setMaxBullets(int maxBullets) {
+
+		this.maxBullets = maxBullets;
+	}
+
+	@Override
+	public void setRechargeTime(double time) {
+
+		this.time = time ;
+	}
+
+	@Override
+	public void setStartingShield(boolean enable) {
+
+		this.shield = enable;
+	}
+
+	@Override
+	public Collection<Spaceship> create(Collection<String> playerIds) {
+		
+		return playerIds.stream()
+				.map( id -> new SpaceshipImpl(speed , maxBullets, shield, id, time))
+				.collect( Collectors.toSet());
+	}
+
+}

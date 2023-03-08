b4a058cb668a3eb44c83fb69d53c3338526a8f53
DarioBerti <dario.berti@studio.unibo.it>
2023-03-08 21:12:28 +0100

    Projectile: aggiunta metodo create

--- src/main/java/it/unibo/AstroParty/model/api/Projectile.java
+++ src/main/java/it/unibo/AstroParty/model/api/Projectile.java
@@ -1,7 +1,12 @@
 package it.unibo.AstroParty.model.api;
 
+import it.unibo.AstroParty.common.Direction;
+import it.unibo.AstroParty.common.Position;
+
 public interface Projectile extends Entity {
     
     @Override
     public CircleHitBox getHitBox();
+    
+    public boolean create(Position SpaceshipPos, Direction SpaceshipDir);
 }

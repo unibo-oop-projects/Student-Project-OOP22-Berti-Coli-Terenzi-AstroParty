1402feaa04e094a233635758b6c19f5bf462b7ec
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2023-03-11 10:58:54 +0100

    game state: collisioni proiettili-ostacoli

--- src/main/java/it/unibo/AstroParty/model/impl/GameStateImpl.java
+++ src/main/java/it/unibo/AstroParty/model/impl/GameStateImpl.java
@@ -9,6 +9,7 @@ import it.unibo.AstroParty.model.api.GameState;
 import it.unibo.AstroParty.model.api.Obstacle;
 import it.unibo.AstroParty.model.api.PowerUp;
 import it.unibo.AstroParty.model.api.Projectile;
+import it.unibo.AstroParty.model.api.RectangleHitBox;
 import it.unibo.AstroParty.model.api.Spaceship;
 
 public class GameStateImpl implements GameState {
@@ -36,7 +37,9 @@ public class GameStateImpl implements GameState {
         spaceships.forEach(obj -> obj.update(time));
         projectiles.forEach(obj -> obj.update(time));
         powerUps.forEach(obj -> obj.update(time));
+        obstacles.forEach(obj -> obj.update(time));
 
+        // controllo le collisioni dei proiettili con i bordi della mappa (in caso li elimino)
         for (Projectile currProjectile : projectiles) {
             r = currProjectile.getHitBox().getRadius();
             pos = currProjectile.getPosition();
@@ -45,9 +48,18 @@ public class GameStateImpl implements GameState {
                     pos.getY() + r > height || pos.getY() - r < 0) {
                 projectiles.remove(currProjectile);
             }
-            //TODO: check collision with obstacles
+            
+            // controllo collisioni proiettile-ostacolo
+            for (Obstacle obs : obstacles) {
+                if (obs.getHitBox().isHitted(pos, r)) {
+                    if (obs.hit()) {
+                        obstacles.remove(obs);
+                    }
+                    projectiles.remove(currProjectile);
+                }
+            }
         }
-
+        
         for (Spaceship currSpaceship : spaceships) {
             r = currSpaceship.getHitBox().getRadius();
             pos = currSpaceship.getPosition();
@@ -55,6 +67,7 @@ public class GameStateImpl implements GameState {
             double fixedX = pos.getX();
             double fixedY = pos.getY();
 
+            // controllo la collisione delle astronavi con i bordi e in caso sistemo la loro posizione
             if (pos.getX() + r > width) {
                 fixedX = width - r;
             } else if (pos.getX() - r < 0) {
@@ -70,8 +83,8 @@ public class GameStateImpl implements GameState {
             if (fixedX != pos.getX() || fixedY != pos.getY()) {
                 currSpaceship.setPosition(new Position(fixedX, fixedY));
             }
-            //TODO: check collision with obstacles
 
+            // controllo le collisioni astronavi-proiettili
             for (Projectile p : projectiles) {
                 if (p.getHitBox().isHitted(pos,r)) {
                     if (currSpaceship.hit()) {

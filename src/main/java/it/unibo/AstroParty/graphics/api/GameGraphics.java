7bb4fc650e5d5f365072e53b2475ef67eb1fd508
Alessandro Coli - alessandro.coli2@studio.unibo.it <alessandro.coli2@studio.unibo.it>
2023-03-21 10:54:02 +0100

    Graphic: implementazione classe che presa una GraphicEntity la trasforma in quanto necessario per disegnarla in javaFX

---
+++ src/main/java/it/unibo/AstroParty/graphics/api/GameGraphics.java
@@ -0,0 +1,7 @@
+package it.unibo.AstroParty.graphics.api;
+
+import it.unibo.AstroParty.graphics.impl.GraphicEntityImpl;
+
+public interface GameGraphics {
+    public void drawEntity( GraphicEntityImpl entity );
+}
---
+++ src/main/java/it/unibo/AstroParty/graphics/impl/matchScene.java
@@ -0,0 +1,10 @@
+package it.unibo.AstroParty.graphics.impl;
+
+import it.unibo.AstroParty.common.Position;
+
+public class matchScene {
+	
+	public void addToScene(Position pos ,double size, double angle, String path) {
+		
+	}
+}

0096ff80eb0b7cbc68ab089ac20d32a66113ca07
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2023-03-24 11:27:16 +0100

    ui: gestione nomi players in SettingsController

--- src/main/java/it/unibo/AstroParty/ui/impl/SettingsController.java
+++ src/main/java/it/unibo/AstroParty/ui/impl/SettingsController.java
@@ -2,8 +2,11 @@ package it.unibo.AstroParty.ui.impl;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import it.unibo.AstroParty.common.Pair;
 import it.unibo.AstroParty.core.api.View;
+import it.unibo.AstroParty.input.api.GameId;
 import it.unibo.AstroParty.ui.api.Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -20,6 +23,7 @@ public class SettingsController implements Controller {
 
     private View app;
     private List<TextField> nameFields;
+    private List<GameId> ids;
 
     @FXML
     private TextField nameP1, nameP2, nameP3, nameP4;
@@ -43,21 +47,29 @@ public class SettingsController implements Controller {
      */
     @FXML
     public void startOnClick(ActionEvent event) {
-        List<String> players = nameFields.stream()
-                .map(t -> t.getText())
-                .filter(n -> !n.isBlank())
+
+        List<Pair<GameId,String>> players = IntStream.range(0,3)
+                .boxed()
+                .map(i -> new Pair<GameId,String>(ids.get(i),nameFields.get(i).getText()))
+                .filter(p -> !p.getY().isBlank())
                 .collect(Collectors.toList());
+
+        // check if the game can start
         if (players.size() < MIN_PLAYERS) {
-            nameFields.stream().forEach(t -> t.setStyle("-fx-border-color: " + (t.getText().isBlank() ? "red" : "black")));
+            nameFields.stream()
+                    .forEach(t -> t.setStyle("-fx-border-color: " + (t.getText().isBlank() ? "red" : "black")));
+        } else {
+            int rounds = roundSelection.getValue();
+            boolean obstacle = obstacleSelection.isSelected();
+            boolean powerUp = powerUpSelection.isSelected();
+            /* 
+            System.out.println("Players: " + players.toString());
+            System.out.println("Rounds: " + rounds
+                    + " Obstacle: " + (obstacle ? "yes" : "no")
+                    + " PowerUp: " + (powerUp ? "yes" : "no"));
+            */
         }
-        int rounds = roundSelection.getValue();
-        boolean obstacle = obstacleSelection.isSelected();
-        boolean powerUp = powerUpSelection.isSelected();
         
-        System.out.println("Players: " + players.toString());
-        System.out.println("Rounds: " + rounds
-                + " Obstacle: " + (obstacle ? "yes" : "no")
-                + " PowerUp: " + (powerUp ? "yes" : "no"));
         // TODO: send the player list and all the other settings to the GameEngine
     }
 
@@ -75,6 +87,7 @@ public class SettingsController implements Controller {
     }
 
     public void initialize() {
+        ids = List.of(GameId.values());
         nameFields = List.of(nameP1, nameP2, nameP3, nameP4);
         nameFields.stream().forEach(t -> t.setPromptText(STARTING_MESSAGE));
         roundSelection.getItems().addAll(ROUND_CHOICES);
@@ -82,3 +95,4 @@ public class SettingsController implements Controller {
     }
 
 }
+

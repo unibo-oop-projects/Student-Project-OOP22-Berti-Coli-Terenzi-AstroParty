adb1eba2e8b50a990fb9d78b727247f9c4a5748a
Mirco Terenzi - mirco.terenzi@studio.unibo.it <mirco.terenzi@studio.unibo.it>
2023-03-30 17:36:54 +0200

    ui: finestre avviso per errori inserimento players

--- src/main/java/it/unibo/AstroParty/ui/impl/SettingsController.java
+++ src/main/java/it/unibo/AstroParty/ui/impl/SettingsController.java
@@ -1,16 +1,18 @@
 package it.unibo.AstroParty.ui.impl;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import it.unibo.AstroParty.core.api.View;
 import it.unibo.AstroParty.ui.api.Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
 public class SettingsController implements Controller {
 
@@ -46,15 +48,27 @@ public class SettingsController implements Controller {
         List<String> players = nameFields.stream()
                 .map(t -> t.getText())
                 .filter(n -> !n.isBlank())
-                .collect(Collectors.toList());
+                .toList();
         if (players.size() < MIN_PLAYERS) {
-            nameFields.stream().forEach(t -> t.setStyle("-fx-border-color: " + (t.getText().isBlank() ? "red" : "black")));
+            showAlert("Non ci sono abbastanza giocatori per iniziare il gioco");
+        } else if (IntStream.range(0, players.size()).anyMatch(i -> nameFields.get(i).getText().isBlank())) {
+            showAlert("Inserisci i nomi dei giocatori in fila (es: non può esserci un player 3 senza prima il player 2)");
+        } else if (players.size() != players.stream().distinct().count()) {
+            showAlert("Sono presenti due player con lo stesso nome");
         } else {
             view.start(players,
                     obstacleSelection.isSelected(),
                     powerUpSelection.isSelected(),
                     roundSelection.getValue());
-        }        
+        }
+    }
+
+    private void showAlert(String message) {
+        final Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle("AstroParty: errore");
+        alert.setHeaderText("Errore nell'inserimento dei players");
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 
     /**

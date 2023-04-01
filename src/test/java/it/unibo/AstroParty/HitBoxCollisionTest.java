33836e8333b88b73333d0ab80a6295ec0a484d33
Mirco Terenzi <mirco.terenzi@studio.unibo.it>
2023-04-01 12:21:43 +0200

    test: correzioni checkstyle a HitBoxCollisionTest

--- src/test/java/it/unibo/AstroParty/HitBoxCollisionTest.java
+++ src/test/java/it/unibo/AstroParty/HitBoxCollisionTest.java
@@ -11,73 +11,85 @@ import it.unibo.AstroParty.model.api.HitBox;
 import it.unibo.AstroParty.model.impl.CircleHitBoxImpl;
 import it.unibo.AstroParty.model.impl.RectangleHitBoxImpl;
 
+/**
+ * a test for the collisions detection.
+ */
+class HitBoxCollisionTest {
 
-public class HitBoxCollisionTest {
-
+    private static final double MAIN_COORD = 0;
     private static final double SIDE = 2;   // the side size of the shapes used in the test
-    private static final double HALF_SIDE = SIDE/2;     
-    private static final double AXIS_DIST = 2*HALF_SIDE;
+    private static final double HALF_SIDE = SIDE / 2;
+    private static final double AXIS_DIST = 2 * HALF_SIDE;
     private static final double CIRCLE_DIAGONAL_DIST = AXIS_DIST;
-    private static final double CIRCLE_DIAGONAL_COMP = CIRCLE_DIAGONAL_DIST/Math.sqrt(2);
-    private static final double RECT_DIAGONAL_DIST = (Math.sqrt(2) * HALF_SIDE) + HALF_SIDE;
-    private static final double RECT_DIAGONAL_COMP = RECT_DIAGONAL_DIST/Math.sqrt(2);
-    
+    private static final double CIRCLE_DIAGONAL_COMP = CIRCLE_DIAGONAL_DIST / Math.sqrt(2);
+    private static final double RECT_DIAGONAL_DIST = Math.sqrt(2) * HALF_SIDE + HALF_SIDE;
+    private static final double RECT_DIAGONAL_COMP = RECT_DIAGONAL_DIST / Math.sqrt(2);
+    private static final double DELTA = 0.1;
+
     @Test
     void testCircleCollisionOnX() {
-        assertTrue(this.testCircleCollision(new CircleHitBoxImpl(new Position(AXIS_DIST - 0.1, 0), HALF_SIDE)));
-        assertFalse(this.testCircleCollision(new CircleHitBoxImpl(new Position(AXIS_DIST + 0.1, 0), HALF_SIDE)));
-        assertTrue(this.testCircleCollision(new CircleHitBoxImpl(new Position(-(AXIS_DIST - 0.1), 0), HALF_SIDE)));
-        assertFalse(this.testCircleCollision(new CircleHitBoxImpl(new Position(-(AXIS_DIST + 0.1), 0), HALF_SIDE)));
+        assertTrue(this.testCircleCollision(this.createCircle(AXIS_DIST - DELTA, MAIN_COORD)));
+        assertFalse(this.testCircleCollision(this.createCircle(AXIS_DIST + DELTA, MAIN_COORD)));
+        assertTrue(this.testCircleCollision(this.createCircle(-(AXIS_DIST - DELTA), MAIN_COORD)));
+        assertFalse(this.testCircleCollision(this.createCircle(-(AXIS_DIST + DELTA), MAIN_COORD)));
     }
 
     @Test
-    void testCircleCollisionOnY() {;
-        assertTrue(this.testCircleCollision(new CircleHitBoxImpl(new Position(0, AXIS_DIST - 0.1), HALF_SIDE)));
-        assertFalse(this.testCircleCollision(new CircleHitBoxImpl(new Position(0, AXIS_DIST + 0.1), HALF_SIDE)));
-        assertTrue(this.testCircleCollision(new CircleHitBoxImpl(new Position(0, -(AXIS_DIST - 0.1)), HALF_SIDE)));
-        assertFalse(this.testCircleCollision(new CircleHitBoxImpl(new Position(0, -(AXIS_DIST + 0.1)), HALF_SIDE)));
+    void testCircleCollisionOnY() {
+        assertTrue(this.testCircleCollision(this.createCircle(MAIN_COORD, AXIS_DIST - DELTA)));
+        assertFalse(this.testCircleCollision(this.createCircle(MAIN_COORD, AXIS_DIST + DELTA)));
+        assertTrue(this.testCircleCollision(this.createCircle(MAIN_COORD, -(AXIS_DIST - DELTA))));
+        assertFalse(this.testCircleCollision(this.createCircle(MAIN_COORD, -(AXIS_DIST + DELTA))));
     }
 
     @Test
     void testCircleCollisionOnDiagonal() {
-        assertTrue(this.testCircleCollision(new CircleHitBoxImpl(new Position(CIRCLE_DIAGONAL_COMP, CIRCLE_DIAGONAL_COMP - 0.1), HALF_SIDE)));
-        assertFalse(this.testCircleCollision(new CircleHitBoxImpl(new Position(-CIRCLE_DIAGONAL_COMP, CIRCLE_DIAGONAL_COMP + 0.1), HALF_SIDE)));
-        assertTrue(this.testCircleCollision(new CircleHitBoxImpl(new Position(CIRCLE_DIAGONAL_COMP - 0.1, CIRCLE_DIAGONAL_COMP - 0.1), HALF_SIDE)));
-        assertFalse(this.testCircleCollision(new CircleHitBoxImpl(new Position(CIRCLE_DIAGONAL_COMP - 0.1, -(CIRCLE_DIAGONAL_COMP + 0.1)), HALF_SIDE)));
+        assertTrue(this.testCircleCollision(this.createCircle(CIRCLE_DIAGONAL_COMP, CIRCLE_DIAGONAL_COMP - DELTA)));
+        assertFalse(this.testCircleCollision(this.createCircle(-CIRCLE_DIAGONAL_COMP, CIRCLE_DIAGONAL_COMP + DELTA)));
+        assertTrue(this.testCircleCollision(this.createCircle(CIRCLE_DIAGONAL_COMP - DELTA, CIRCLE_DIAGONAL_COMP - DELTA)));
+        assertFalse(this.testCircleCollision(this.createCircle(CIRCLE_DIAGONAL_COMP - DELTA, -(CIRCLE_DIAGONAL_COMP + DELTA))));
     }
 
     @Test
     void testRectCollisionOnX() {
-        assertTrue(this.testCircleCollision(new RectangleHitBoxImpl(this.createRectPosition(AXIS_DIST - 0.1, 0), SIDE, SIDE)));
-        assertFalse(this.testCircleCollision(new RectangleHitBoxImpl(this.createRectPosition(AXIS_DIST + 0.1, 0), SIDE, SIDE)));
-        assertTrue(this.testCircleCollision(new RectangleHitBoxImpl(this.createRectPosition(-(AXIS_DIST - 0.1), 0), SIDE, SIDE)));
-        assertFalse(this.testCircleCollision(new RectangleHitBoxImpl(this.createRectPosition(-(AXIS_DIST + 0.1), 0), SIDE, SIDE)));
+        assertTrue(this.testCircleCollision(this.createRectangle(AXIS_DIST - DELTA, MAIN_COORD)));
+        assertFalse(this.testCircleCollision(this.createRectangle(AXIS_DIST + DELTA, MAIN_COORD)));
+        assertTrue(this.testCircleCollision(this.createRectangle(-(AXIS_DIST - DELTA), MAIN_COORD)));
+        assertFalse(this.testCircleCollision(this.createRectangle(-(AXIS_DIST + DELTA), MAIN_COORD)));
     }
 
     @Test
-    void testRectCollisionOnY() {;
-        assertTrue(this.testCircleCollision(new RectangleHitBoxImpl(this.createRectPosition(0, AXIS_DIST - 0.1), SIDE, SIDE)));
-        assertFalse(this.testCircleCollision(new RectangleHitBoxImpl(this.createRectPosition(0, AXIS_DIST + 0.1), SIDE, SIDE)));
-        assertTrue(this.testCircleCollision(new RectangleHitBoxImpl(this.createRectPosition(0, -(AXIS_DIST - 0.1)), SIDE, SIDE)));
-        assertFalse(this.testCircleCollision(new RectangleHitBoxImpl(this.createRectPosition(0, -(AXIS_DIST + 0.1)), SIDE, SIDE)));
+    void testRectCollisionOnY() {
+        assertTrue(this.testCircleCollision(this.createRectangle(MAIN_COORD, AXIS_DIST - DELTA)));
+        assertFalse(this.testCircleCollision(this.createRectangle(MAIN_COORD, AXIS_DIST + DELTA)));
+        assertTrue(this.testCircleCollision(this.createRectangle(MAIN_COORD, -(AXIS_DIST - DELTA))));
+        assertFalse(this.testCircleCollision(this.createRectangle(MAIN_COORD, -(AXIS_DIST + DELTA))));
     }
 
     @Test
     void testRectCollisionOnDiagonal() {
-        assertTrue(this.testCircleCollision(new RectangleHitBoxImpl(this.createRectPosition(RECT_DIAGONAL_COMP, RECT_DIAGONAL_COMP - 0.1), SIDE, SIDE)));
-        assertFalse(this.testCircleCollision(new RectangleHitBoxImpl(this.createRectPosition(-(RECT_DIAGONAL_COMP + 0.1), RECT_DIAGONAL_COMP + 0.1), SIDE, SIDE)));
-        assertTrue(this.testCircleCollision(new RectangleHitBoxImpl(this.createRectPosition(RECT_DIAGONAL_COMP - 0.1, RECT_DIAGONAL_COMP - 0.1), SIDE, SIDE)));
-        assertFalse(this.testCircleCollision(new RectangleHitBoxImpl(this.createRectPosition(RECT_DIAGONAL_COMP, -(RECT_DIAGONAL_COMP + 0.1)), SIDE, SIDE)));
+        assertTrue(this.testCircleCollision(this.createRectangle(RECT_DIAGONAL_COMP, RECT_DIAGONAL_COMP - DELTA)));
+        assertFalse(this.testCircleCollision(this.createRectangle(-(RECT_DIAGONAL_COMP + DELTA), RECT_DIAGONAL_COMP + DELTA)));
+        assertTrue(this.testCircleCollision(this.createRectangle(RECT_DIAGONAL_COMP - DELTA, RECT_DIAGONAL_COMP - DELTA)));
+        assertFalse(this.testCircleCollision(this.createRectangle(RECT_DIAGONAL_COMP, -(RECT_DIAGONAL_COMP + DELTA))));
     }
 
     // check for collisions with a circle in [0,0]
-    private boolean testCircleCollision(HitBox hBox) {
-        final CircleHitBox circle = new CircleHitBoxImpl(new Position(0, 0),HALF_SIDE);
+    private boolean testCircleCollision(final HitBox hBox) {
+        final CircleHitBox circle = new CircleHitBoxImpl(new Position(MAIN_COORD, MAIN_COORD), HALF_SIDE);
         return hBox.checkCircleCollision(circle);
     }
 
-    // create a ULCorner position (for the RectangleHitBox constructor) from a central position
-    private Position createRectPosition(double x, double y) {
+    private HitBox createCircle(final double x, final double y) {
+        return new CircleHitBoxImpl(new Position(x, y), HALF_SIDE);
+    }
+
+    private HitBox createRectangle(final double x, final double y) {
+        return new RectangleHitBoxImpl(this.getRectPosition(x, y), SIDE, SIDE);
+    }
+
+    // get the ULCorner position (for the RectangleHitBox constructor) from a central position
+    private Position getRectPosition(final double x, final double y) {
         return new Position(x - HALF_SIDE, y - HALF_SIDE);
     }
 }

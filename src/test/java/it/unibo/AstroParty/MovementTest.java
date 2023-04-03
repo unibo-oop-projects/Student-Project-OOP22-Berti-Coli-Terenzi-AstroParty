9630a6e8a4388758d11a77830620b76fc713add7
Alessandro Coli - alessandro.coli2@studio.unibo.it <alessandro.coli2@studio.unibo.it>
2023-04-03 10:39:53 +0200

    JavaDoc

--- src/test/java/it/unibo/AstroParty/MovementTest.java
+++ src/test/java/it/unibo/AstroParty/MovementTest.java
@@ -1,8 +1,6 @@
 package it.unibo.AstroParty;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 import it.unibo.AstroParty.common.Direction;
@@ -10,6 +8,9 @@ import it.unibo.AstroParty.common.Position;
 import it.unibo.AstroParty.model.Spaceship.impl.SpaceshipImpl;
 import it.unibo.AstroParty.model.api.Spaceship;
 
+/**
+ * test for the movemnt of a spaceship
+ */
 public class MovementTest {
     private static final double startAngle = 45;
     private static final Position startPos = new Position(10, 10);
@@ -18,6 +19,9 @@ public class MovementTest {
     private static final double speed = 1;
     private final Spaceship spaceship =  new SpaceshipImpl(startPos, startDir, startAngle, null, speed, 0, false, null, 0);
 
+    /**
+     * test the update when going straight
+     */
     @Test
     void testMovementForward() {      
         this.spaceship.resetPosition();
@@ -26,6 +30,9 @@ public class MovementTest {
         assertEquals( expectedPosition , this.spaceship.getPosition() );
     }
 
+    /**
+     * test update when turning
+     */
     @Test
     void testTurn() {      
         this.spaceship.resetPosition();
